97773881a205daf3c663a217ead68227712a1a4f
Maninder Singh <manindersolitaire@gmail.com>
2026-06-02 08:49:05 +0530

    Fix dashboard route naming mismatch

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboardController');
 
-router.get('/stats', dashboardController.getStats);
+router.get('/stats', dashboardController.getDashboardStats);
 
 module.exports = router;
